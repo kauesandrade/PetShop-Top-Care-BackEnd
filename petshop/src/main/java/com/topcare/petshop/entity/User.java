a66cb6eb5aaa55699ff66e8225cce256f2554571
kauesandrade <128431286+kauesandrade@users.noreply.github.com>
2024-07-11 21:56:49 -0300

    init project

---
+++ petshop/src/main/java/com/topcare/petshop/entity/User.java
@@ -0,0 +1,21 @@
+package com.topcare.petshop.entity;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String fullname;
+
+}
