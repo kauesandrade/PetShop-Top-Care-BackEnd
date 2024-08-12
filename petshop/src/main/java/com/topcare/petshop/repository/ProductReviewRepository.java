62c32a613b190f84efb2b084b9c6ad4708756a98
Bernardo P Zapelini <bernardozplz@gmail.com>
2024-08-12 18:01:46 -0300

    adjustiments

--- petshop/src/main/java/com/topcare/petshop/repository/ProductReviewRepository.java
+++ petshop/src/main/java/com/topcare/petshop/repository/ProductReviewRepository.java
@@ -1,9 +1,15 @@
 package com.topcare.petshop.repository;
 
+import com.topcare.petshop.controller.dto.productReview.ProductReviewGetDTO;
 import com.topcare.petshop.entity.ProductReview;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface ProductReviewRepository extends JpaRepository<ProductReview, Long> {
+
+    List<ProductReviewGetDTO> findBy
+
 }
