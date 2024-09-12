d2548d79ced3b4ebfb095ebf20a815afb3cb478d
Bernardo P Zapelini <bernardozplz@gmail.com>
2024-09-12 14:03:52 -0300

    Adding Cross Origin at UserController

--- petshop/src/main/java/com/topcare/petshop/controller/UserController.java
+++ petshop/src/main/java/com/topcare/petshop/controller/UserController.java
@@ -17,6 +17,7 @@ import org.springframework.web.bind.annotation.*;
 @Controller
 @AllArgsConstructor
 @RequestMapping("/topcare/user")
+@CrossOrigin("*")
 public class UserController {
 
     private final UserServiceImpl service;
